6102ea6579c72752eb1b0aebde81f15890fca525
jezereal <jezerealgilbert@gmail.com>
2023-03-25 13:36:52 -0700

    main.h

--- main.h
+++ main.h
@@ -4,4 +4,41 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int _printf(const char *format, ...);
+int _printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    int num_chars_printed = 0;
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            if (*format == '\0') {
+                break;
+            }
+            if (*format == '%') {
+                putchar('%');
+                num_chars_printed++;
+            } else if (*format == 'c') {
+                char c = va_arg(args, int);
+                putchar(c);
+                num_chars_printed++;
+            } else if (*format == 's') {
+                char *str = va_arg(args, char *);
+                while (*str != '\0') {
+                    putchar(*str);
+                    str++;
+                    num_chars_printed++;
+                }
+            }
+        } else {
+            putchar(*format);
+            num_chars_printed++;
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return num_chars_printed;
+}
